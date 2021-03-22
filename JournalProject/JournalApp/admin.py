e7b63e45d81dda9806aac599094ef690a0fb143b
Ray Aeron <rayaeron95@hotmail.com>
2021-03-23 01:20:03 +0300

    f

--- JournalProject/JournalApp/admin.py
+++ JournalProject/JournalApp/admin.py
@@ -7,23 +7,23 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = User
-    list_display = ('email', 'is_staff', 'is_active',)
-    list_filter = ('email', 'is_staff', 'is_active',)
+    list_display = ('email', 'is_staff', 'is_active','date_joined','fk_group')
+    list_filter = ('email', 'is_staff', 'is_active','date_joined','fk_group')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active','date_joined','fk_group')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'date_joined','fk_group')}
         ),
     )
     search_fields = ('email',)
     ordering = ('email',)
 
 
-admin.site.register(User)
+admin.site.register(User,CustomUserAdmin)
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
